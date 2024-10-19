f6d4f53154220389dfa7d2abc03446d1818d85b5
Wallisson Pereira <157427235+WalliPereira@users.noreply.github.com>
2024-10-18 22:51:12 -0300

    Update crapsGame.js

--- JavaScript/MiniGame/crapsGame.js
+++ JavaScript/MiniGame/crapsGame.js
@@ -1,3 +1,11 @@
+// Objetivo: O jogador lança dois dados e tenta ganhar baseado em regras específicas.
+// Regras:
+
+// Ganhar: Se a primeira jogada resulta em 7 ou 11, o jogador ganha imediatamente (natural).
+// Perder: Se a primeira jogada resulta em 2, 3 ou 12, o jogador perde (craps).
+// Estabelecer um Ponto: Se a primeira jogada resulta em 4, 5, 6, 8, 9 ou 10, esse número se torna o "ponto".
+// O jogador continua lançando até tirar o mesmo ponto novamente (ganha) ou um 7 (perde).
+
 function lancarDados() {
     return Math.floor(Math.random() * 6) + 1 + Math.floor(Math.random() * 6) + 1;
 }
